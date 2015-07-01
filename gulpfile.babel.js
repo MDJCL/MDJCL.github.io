7eac2278d753bae830cdcf8faddbe23f17ae7720
Jonah Langlieb <jonahlanglieb@gmail.com>
2015-07-01 15:14:57 -0400

    next

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,14 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 import del from 'del';
+
 import {
     stream as wiredep
 }
 from 'wiredep';
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
+const runSequence = require("run-sequence")
 gulp.task('styles', () => {
     return gulp.src('app/styles/*.css').pipe($.sourcemaps.init()).pipe($.autoprefixer({
         browsers: ['last 1 version']
@@ -38,8 +40,23 @@ gulp.task('html', ['styles'], () => {
         loose: true
     }))).pipe(gulp.dest('dist'));
 });
+gulp.task('resizeGallery', () => {
+    return gulp.src('app/images/miscGallery/*').pipe($.responsive({
+        '*': {
+            width: 434,
+            height: 290,
+            rename: {
+                suffix: "-resized"
+            },
+            withoutEnlargement: false
+        }
+    }, {
+        errorOnUnusedImage: false
+    })).pipe(gulp.dest('app/images/resized/miscGallery'))
+})
+
 gulp.task('resize', () => {
-    return gulp.src('app/images/**/*').pipe($.responsive({
+    return gulp.src('app/images/*').pipe($.responsive({
         '{Laurel.jpg,Watch.jpg,Help.jpg,Nervs.jpg}': {
             width: 430,
             height: 287,
@@ -53,13 +70,63 @@ gulp.task('resize', () => {
             rename: {
                 suffix: "-resized"
             }
+        },
+        '{Chariot.jpg,}': {
+            width: 325,
+            height: 433,
+            rename: {
+                suffix: "-resized"
+            }
+        },
+        '{OfficerElection.png,ConventionSchedule.png,Vulpes.png}': {
+            width: "100%",
+            height: 522,
+            rename: {
+                suffix: "-resized"
+            },
+            withoutEnlargement: false
+        },
+        'Confusion*': {
+            width: 654,
+            height: 437,
+            rename: {
+                suffix: "-resized"
+            },
+            withoutEnlargement: false
+        },
+        '{Registration.png,Constit.png}': {
+            width: "100%",
+            height: "100%",
+            rename: {
+                suffix: "-resized"
+            }
         }
+
     }, {
         errorOnUnusedImage: false
-    })).pipe(gulp.dest('app/images'))
+    })).pipe(gulp.dest('app/images/resized'))
 })
+gulp.task('deploy', function() {
+    //build needs to be already done
+    return gulp.src("{./dist/**/*,./Docs/**/*}").pipe($.ghPages())
+});
+gulp.task('spellcheck', function() {
+    // throw "didn't work"
+    return gulp.src('app/index.html')
+        // .pipe($.util.log("aspell check -H /home/jonah/Dropbox/Public/MDJCL/yeoman/index.html"))
+        .pipe($.spellcheck({
+            "stdout": true,
+            "mode": "html",
+            "ignore": ["th", "Linganore" , "Ballenger"]
+        }))
+        .pipe($.prompt.confirm({
+                 message: 'Continue? If not aspell check -H /home/jonah/Dropbox/Public/MDJCL/yeoman/index.html',
+        default: true
+        }))
+        // .pipe(gulp.dest('app'));
+});
 gulp.task('images', ['resize'], () => {
-    return gulp.src('app/images/*-resized*').pipe($.if($.if.isFile, $.cache($.imagemin({
+    return gulp.src('app/images/resized/**/*').pipe($.if($.if.isFile, $.cache($.imagemin({
         progressive: true,
         interlaced: true,
         // don't remove IDs from SVGs, they are often used
@@ -72,7 +139,7 @@ gulp.task('images', ['resize'], () => {
     }).on('error', function(err) {
         console.log(err);
         this.end();
-    })).pipe(gulp.dest('dist/images'));
+    })).pipe(gulp.dest('dist/images/resized'));
 });
 gulp.task('clear', function(done) {
     return $.cache.clearAll(done);
@@ -95,15 +162,18 @@ gulp.task('serve', ['styles', 'fonts', 'images'], () => {
         server: {
             baseDir: ['.tmp', 'app'],
             routes: {
-                '/bower_components': 'bower_components'
+                '/bower_components': 'bower_components',
+                '/Docs': 'Docs'
             }
         }
     });
     gulp.watch(['app/*.html', 'app/scripts/**/*.js', '.tmp/fonts/**/*']).on('change', reload);
     //jonah
-    gulp.watch('app/images/**/*', ['resize']);
+    gulp.watch('app/images/*', ['resize']);
     //end jonah
-    gulp.watch('app/styles/**/*.css', ['styles']);
+    gulp.watch('app/styles/**/*.css', {
+        debounceDelay: 2000
+    }, ['styles']);
     gulp.watch('app/fonts/**/*', ['fonts']);
     gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
@@ -140,12 +210,67 @@ gulp.task('wiredep', () => {
         ignorePath: /^(\.\.\/)*\.\./
     })).pipe(gulp.dest('app'));
 });
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-    return gulp.src('dist/**/*').pipe($.size({
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], (cb) => {
+    gulp.src('dist/**/*').pipe($.size({
         title: 'build',
         gzip: true
     }));
+    cb("")
 });
-gulp.task('default', ['clean'], () => {
-    gulp.start('build');
+gulp.task('noCritical', (callback) => {
+    runSequence('clean', 'build', 'deploy', callback);
+});
+gulp.task('default', function(callback) {
+    runSequence(['clean', 'spellcheck'], 'build', 'critical', 'deploy', callback);
+});
+gulp.task('copystyles', function() {
+    return gulp.src(['dist/styles/main.css']).pipe($.rename({
+        basename: "site" // site.css
+    })).pipe(gulp.dest('dist/styles'));
+});
+gulp.task('critical', ['copystyles'], function(cb) {
+
+    return gulp.src('dist/*.html')
+        .pipe(require('critical').stream({
+            // Inline the generated critical-path CSS
+            // - true generates HTML
+            // - false generates CSS
+            inline: true,
+
+
+            // Your base directory
+            base: 'dist/',
+
+            // HTML source
+
+            // HTML source file
+            // src: 'index.html',
+
+            // Target for final HTML output.
+            // use some css file when the inline option is not set
+            // dest: 'index-critical.html',
+
+            // // Minify critical-path CSS when inlining
+            minify: true,
+
+            // // Extract inlined styles from referenced stylesheets
+            extract: true,
+
+            // Prefix for asset directory
+
+            // ignore css rules
+            ignore: ['@font-face']
+        })).pipe(gulp.dest('dist'));
+
+    // critical.generateInline({
+    //     inline: true,
+    //     base: 'dist/',
+    //     src: 'index.html',
+    //     // htmlTarget: 'index.html',
+    //     dest: 'index-critical.html',
+    //     minify: true,
+    //     width: 1300,
+    //     height: 900,
+    //      ignore: ['@font-face', /url\(/,/SIFONN_BASIC/g, 'font', /font/, /font/g, /@font-face/g]
+    // }, cb.bind(cb));
 });
