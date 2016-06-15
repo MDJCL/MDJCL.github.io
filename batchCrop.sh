06154b47094139818189cbef44fc8e25d09e0be1
Jonah <jonahmail1@gmail.com>
2016-06-15 16:54:08 -0400

    Finishing up initial webmaster's reign

--- batchCrop.sh
+++ batchCrop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(ls images/*);do
-        node ./node_modules/smartcrop-cli/smartcrop-cli.js --width 651 --height 435 --quality 90 $i outputImages/$i
+for i in $(ls /home/jonah/Dropbox/Public/MDJCL/app/images/miscGallery/*);do
+        node /home/jonah/Dropbox/Public/MDJCL/node_modules/smartcrop-cli/smartcrop-cli.js --width 651 --height 435 --quality 90 $i /home/jonah/Dropbox/Public/MDJCL/app/images/miscGallery/batchCrop/`echo $i|sed "s/.*\///g"`
         echo $i
 done
